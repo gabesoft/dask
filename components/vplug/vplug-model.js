2cc3d0cf355e012a0917e9a57b11614dc031c882
Gabriel Adomnicai <gabesoft@gmail.com>
2015-04-12 23:04:37 -0700

    vplug model fields

--- components/vplug/vplug-model.js
+++ components/vplug/vplug-model.js
@@ -9,12 +9,13 @@ var mongoose = require('mongoose')
  * Model that represents a vim plugin
  */
 Vplug = schema.create({
-    name        : String
-  , author      : String
-  , githubUrl   : { type: String, index: { unique: true, sparse: true } }
-  , vimorgUrl   : { type: String, index: { unique: true, sparse: true } }
-  , tags        : { type: [String], index: true }
-  , description : String
+    name            : String
+  , author          : String
+  , githubUrl       : { type: String, index: { unique: true, sparse: true } }
+  , githubStarCount : Number
+  , vimorgUrl       : { type: String, index: { unique: true, sparse: true } }
+  , tags            : { type: [String], index: true }
+  , description     : String
 });
 
 module.exports = mongoose.model('Vplug', Vplug);
