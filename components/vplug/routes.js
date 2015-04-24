3de01c6a782bd219ab9535ccdf7d123ed6a83c63
Gabriel Adomnicai <gabesoft@gmail.com>
2015-04-23 23:18:47 -0700

    plugin keywords routes

--- components/vplug/routes.js
+++ components/vplug/routes.js
@@ -2,6 +2,37 @@
 
 var handler = require('./handlers');
 
+function makeKey (key) {
+    return 'vplugkeyword:' + key;
+}
+
+function setKeywords (request, reply) {
+    var redis   = request.server.app.redis
+      , payload = request.payload
+      , data    = [];
+
+    payload.forEach(function (d) {
+        data.push(makeKey(d.key));
+        data.push(JSON.stringify(d));
+    });
+
+    redis.mset(data, function (err, res) {
+        return err ? reply.boom(err) : reply(res);
+    });
+}
+
+function getKeywords (request, reply) {
+    var redis = request.server.app.redis;
+
+    redis.keys(makeKey('*'), function (err, keys) {
+        if (err) return reply.boom(err);
+
+        redis.mget(keys, function (err, data) {
+            return err ? reply.boom(err) : reply(data.map(JSON.parse));
+        });
+    });
+}
+
 module.exports = [{
     method  : 'POST'
   , path    : '/vplugs'
@@ -18,4 +49,12 @@ module.exports = [{
     method  : [ 'PUT', 'PATCH' ]
   , path    : '/vplugs/{id}'
   , handler : handler.update
+}, {
+    method  : 'POST'
+  , path    : '/vplugkeywords'
+  , handler : setKeywords
+}, {
+    method  : 'GET'
+  , path    : '/vplugkeywords'
+  , handler : getKeywords
 }]
