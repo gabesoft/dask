378633c1ef1e813fae71d3d89a8deee7676242fc
Gabriel Adomnicai <gabesoft@gmail.com>
2015-06-13 15:22:25 -0700

    Vplug model additions

--- components/vplug/vplug-model.js
+++ components/vplug/vplug-model.js
@@ -10,16 +10,20 @@ var mongoose = require('mongoose')
 Vplug = schema.create({
     author          : { type : Object }
   , description     : { type : String }
-  , githubStarCount : { type : Number }
-  , isPlugin        : { type : Number, default : 0 }
-  , githubUrl       : { type : String, index: { unique: true, sparse: true } }
-  , name            : { type : String }
-  , tags            : { type : [String], index: true }
-  , vimorgUrl       : { type : String, index: { unique: true, sparse: true } }
-  , readme          : { type : Object }
   , doc             : { type : Object }
+  , githubCloneUrl  : { type : String }
+  , githubCreatedAt : { type : Date }
+  , githubHtmlUrl   : { type : String, index: { unique: true, sparse: true } }
+  , githubPushedAt  : { type : Date }    // date and time of the last commit
+  , githubStarCount : { type : Number }
+  , githubUpdatedAt : { type : Date }    // date and time of the last change
   , hasDoc          : { type : Boolean, default : false  }
   , hasReadme       : { type : Boolean, default : false }
+  , isPlugin        : { type : Number, default : 0 }
+  , name            : { type : String }
+  , readme          : { type : Object }
+  , tags            : { type : [String], index: true }
+  , vimorgUrl       : { type : String, index: { unique: true, sparse: true } }
 });
 
 Vplug.index({
