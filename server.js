66d3fda61420530a903217b63133e04f5c03043b
Gabriel Adomnicai <gabesoft@gmail.com>
2015-10-29 21:47:47 -0700

    Dependency update + file re-format

--- server.js
+++ server.js
@@ -1,62 +1,66 @@
 'use strict';
 
-var path     = require('path')
-  , glob     = require('glob')
-  , async    = require('async')
-  , mongoose = require('mongoose')
-  , redis    = require('redis')
-  , conf     = require('./config/store.js')
-  , Hapi     = require('hapi')
-  , server   = new Hapi.Server({});
+var path = require('path'),
+    glob = require('glob'),
+    async = require('async'),
+    mongoose = require('mongoose'),
+    redis = require('redis'),
+    conf = require('./config/store.js'),
+    Hapi = require('hapi'),
+    server = new Hapi.Server({});
 
-function connectMongoose (cb) {
-    var host = conf.get('mongo:host')
-      , db   = conf.get('mongo:database')
-      , port = conf.get('mongo:port')
-      , url  = 'mongodb://' + host + ':' + port + '/' + db;
+function connectMongoose(cb) {
+    var host = conf.get('mongo:host'),
+        db = conf.get('mongo:database'),
+        port = conf.get('mongo:port'),
+        url = 'mongodb://' + host + ':' + port + '/' + db;
     mongoose.connect(url, cb);
 }
 
-function connectRedis (cb) {
-    var port   = conf.get('redis:port')
-      , host   = conf.get('redis.host')
-      , opts   = {}
-      , client = redis.createClient(port, host, opts);
+function connectRedis(cb) {
+    var port = conf.get('redis:port'),
+        host = conf.get('redis.host'),
+        opts = {},
+        client = redis.createClient(port, host, opts);
 
-    client.on('ready', function () {
+    client.on('ready', function() {
         server.app.redis = client;
         cb();
     });
     client.on('error', cb);
-    client.on('error', function (err) {
+    client.on('error', function(err) {
         console.log(err);
     });
 }
 
-function setupServer (cb) {
-    server.connection({ port: conf.get('app:port') || 8006 });
+function setupServer(cb) {
+    server.connection({
+        port: conf.get('app:port') || 8006
+    });
     cb(null);
 }
 
-function loadRoutes (cb) {
-    var components = path.join(__dirname, 'components')
-      , opts       = {
-            nomount : false
-          , cwd     : components
-          , root    : components
+function loadRoutes(cb) {
+    var components = path.join(__dirname, 'components'),
+        opts = {
+            nomount: false,
+            cwd: components,
+            root: components
         };
 
-    glob('/**/routes.js', opts, function (err, files) {
-        if (err) { return cb(err); }
+    glob('/**/routes.js', opts, function(err, files) {
+        if (err) {
+            return cb(err);
+        }
 
-        files.forEach(function (file) {
+        files.forEach(function(file) {
             var routes = require(file);
 
             if (!Array.isArray(routes)) {
                 routes = [routes];
             }
 
-            routes.forEach(function (route) {
+            routes.forEach(function(route) {
                 try {
                     server.route(route);
                 } catch (e) {
@@ -69,39 +73,36 @@ function loadRoutes (cb) {
     });
 }
 
-function registerPlugins (cb) {
+function registerPlugins(cb) {
     server.register([{
-        register : require('http-status-decorator')
+        register: require('http-status-decorator')
     }, {
-        register : require('good')
-      , options  : {
-            logRequestPayload  : false
-          , logResponsePayload : false
-          , reporters          : [{
-                reporter : require('good-console')
-              , args     : [
-                    { log: '*', response : '*', error: '*' }
-                  , { format: 'hh:mm:ss.SSS' } ]
+        register: require('good'),
+        options: {
+            reporters: [{
+                reporter: require('good-console'),
+                events: [{
+                    log: '*',
+                    response: '*',
+                    error: '*'
+                }, {
+                    format: 'hh:mm:ss.SSS'
+                }]
             }]
         }
     }], cb);
 }
 
-function startServer (cb) {
-    server.start(function (err) {
+function startServer(cb) {
+    server.start(function(err) {
         console.log('server started: ', server.info.uri);
         cb(err);
     });
 }
 
 async.series([
-    setupServer
-  , loadRoutes
-  , registerPlugins
-  , connectMongoose
-  , connectRedis
-  , startServer
-], function (err) {
+    setupServer, loadRoutes, registerPlugins, connectMongoose, connectRedis, startServer
+], function(err) {
     if (err) {
         console.log(err);
         throw err;
