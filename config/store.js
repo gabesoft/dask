d006ad9261496e77ba313402c849e0f891d6e285
Gabriel Adomnicai <gabesoft@gmail.com>
2016-10-27 00:16:33 -0700

    Configuration fixes

--- config/store.js
+++ config/store.js
@@ -3,19 +3,21 @@
 const nconf = require('nconf'),
       path = require('path'),
       env = process.env.NODE_ENV || 'development',
-      root = process.cwd();
+      dir = __dirname,
+      configPath = (name) => `${path.join(dir, name)}.json`,
+      defaultPath = configPath('default');
 
 nconf.overrides({
   env,
   path: {
-    root,
-    config: process.env.DASK_CONFIG_PATH || (path.join(root, 'config', env) + '.json')
+    root: path.join(dir, '..'),
+    config: process.env.DASK_CONFIG_PATH || configPath(env)
   }
 });
 
 nconf.env();
 nconf.argv();
 nconf.file(env, nconf.get('path:config'));
-nconf.defaults(require('./default.json'));
+nconf.defaults(require(defaultPath));
 
 module.exports = nconf;
